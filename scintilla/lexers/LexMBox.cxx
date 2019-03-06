2b42a2da9182facbb70a9d583fea712c8b2e74d0
dusan96 <dusan.pantelic96@gmail.com>
2019-03-06 14:41:34 +0100

    Fixed lockup issue

--- scintilla/lexers/LexMBox.cxx
+++ scintilla/lexers/LexMBox.cxx
@@ -32,11 +32,11 @@ static const char *const MBoxWordlistDesc[] = {
     0
 };
 
-// Map of real line states 
-std::map<Sci_Position, char> dataMap;
+// Map of real line states for current document in editor
+std::map<IDocument*, std::map<Sci_Position, char>> documentDataMap;
 
-// Map of line states when processed, this is used for highlight
-std::map<Sci_Position, char> stateMap;
+// Map of line states when processed for current document in editor, this is used for highlight
+std::map<IDocument*, std::map<Sci_Position, char>> documentStateMap;
 
 // Great performance emprovement when declaring regex only once
 std::regex customKeywordRegex("^\\S+:.*?\\s+");
@@ -175,50 +175,53 @@ bool IsCustomKeywordValueAcrossLine(std::string line) {
 }
 
 // Process text and for each line add state in dataMap 
-Sci_Position ProcessLines(Sci_Position startPos, Sci_Position lengthDoc, LexAccessor &styler) {
+Sci_Position ProcessLines(Sci_Position startPos, Sci_Position lengthDoc, LexAccessor &styler, IDocument *pAccess) {
 
     std::string lineBuffer;
     Sci_Position currentPos = startPos;
     Sci_Position currentLine = styler.GetLine(startPos);
     Sci_Position endDoc = styler.Length();
 
-    while((currentPos < startPos+lengthDoc || dataMap[currentLine-1] != SCE_MBOX_FROM) && currentPos <= endDoc ) {
+    while((currentPos < startPos+lengthDoc || documentDataMap[pAccess][currentLine-1] != SCE_MBOX_FROM) && currentPos <= endDoc ) {
 
         char c = static_cast<char>(styler.SafeGetCharAt(currentPos++));
         lineBuffer += c;
 
-        if (c == '\n') {
+        if (c == '\n' || currentPos >= endDoc) {
             
             if (lineBuffer.substr(0,5) == "From " && IsFromLine(lineBuffer)) {
-                dataMap[currentLine] = SCE_MBOX_FROM;
+                documentDataMap[pAccess][currentLine] = SCE_MBOX_FROM;
             }
             else if (lineBuffer.find(':') != std::string::npos && IsCustomKeywordLine(lineBuffer)) {
-                dataMap[currentLine] = SCE_MBOX_CUSTOM_KEYWORD;
+                documentDataMap[pAccess][currentLine] = SCE_MBOX_CUSTOM_KEYWORD;
             }
             else if (lineBuffer == "\n" || lineBuffer == "\r\n") {
-                dataMap[currentLine] = SCE_MBOX_BLANK_LINE;
+                documentDataMap[pAccess][currentLine] = SCE_MBOX_BLANK_LINE;
             }
-            else if (dataMap.find(currentLine-1) != dataMap.end() && (dataMap[currentLine-1] == SCE_MBOX_CUSTOM_KEYWORD || dataMap[currentLine-1] == SCE_MBOX_CUSTOM_KEYWORD_VALUE) && IsCustomKeywordValueAcrossLine(lineBuffer)) {
-                dataMap[currentLine] = SCE_MBOX_CUSTOM_KEYWORD_VALUE;
+            else if (documentDataMap[pAccess].find(currentLine-1) != documentDataMap[pAccess].end() && (documentDataMap[pAccess][currentLine-1] == SCE_MBOX_CUSTOM_KEYWORD || documentDataMap[pAccess][currentLine-1] == SCE_MBOX_CUSTOM_KEYWORD_VALUE) && IsCustomKeywordValueAcrossLine(lineBuffer)) {
+                documentDataMap[pAccess][currentLine] = SCE_MBOX_CUSTOM_KEYWORD_VALUE;
             }
             else {
-                    dataMap[currentLine] = SCE_MBOX_DEFAULT;
+                    documentDataMap[pAccess][currentLine] = SCE_MBOX_DEFAULT;
             }
 
             lineBuffer.clear();
             currentLine++;
         } 
     }
+    if (currentLine <= styler.GetLine(startPos)) {
+        return styler.GetLine(startPos);
+    }
 
     return currentLine-1;
 }
 
 // Process lines and states from dataMap and form stateMap wich is used for highlight
-void ProcessStates(Sci_Position startLine, Sci_Position endLine) {
+void ProcessStates(Sci_Position startLine, Sci_Position endLine, IDocument *pAccess) {
 
-    for (auto data = dataMap.find(startLine); data->first <= endLine; data++) {
+    for (auto data = documentDataMap[pAccess].find(startLine); data != documentDataMap[pAccess].end() && data->first <= endLine; data++) {
 
-        stateMap[data->first] = SCE_MBOX_DEFAULT;
+        documentStateMap[pAccess][data->first] = SCE_MBOX_DEFAULT;
         
         if (data->first != 0) {
             if (data->second != SCE_MBOX_BLANK_LINE) {
@@ -232,22 +235,22 @@ void ProcessStates(Sci_Position startLine, Sci_Position endLine) {
 
         Sci_Position begin = data->first;
         Sci_Position end = data->first;
-        while (data != dataMap.end() && data->second != SCE_MBOX_BLANK_LINE) {
+        while (data != documentDataMap[pAccess].end() && data->second != SCE_MBOX_BLANK_LINE) {
             end = data->first;
             data++;
         }
         data--;
         
-        if (dataMap[begin] != SCE_MBOX_FROM) {
+        if (documentDataMap[pAccess][begin] != SCE_MBOX_FROM) {
             for (Sci_Position i = begin; i <= end; i++) {
-                stateMap[i] = SCE_MBOX_DEFAULT;
+                documentStateMap[pAccess][i] = SCE_MBOX_DEFAULT;
             }
             continue;
         }
 
         bool isAllKeywords = true;
         for (Sci_Position i = begin+1; i <= end; i++) {
-            if (dataMap[i] != SCE_MBOX_CUSTOM_KEYWORD && dataMap[i] != SCE_MBOX_CUSTOM_KEYWORD_VALUE) {
+            if (documentDataMap[pAccess][i] != SCE_MBOX_CUSTOM_KEYWORD && documentDataMap[pAccess][i] != SCE_MBOX_CUSTOM_KEYWORD_VALUE) {
                 isAllKeywords = false;
                 break;
             }
@@ -255,13 +258,20 @@ void ProcessStates(Sci_Position startLine, Sci_Position endLine) {
 
         if (!isAllKeywords) {
             for (Sci_Position i = begin; i <= end; i++) {
-                stateMap[i] = SCE_MBOX_DEFAULT;
+                documentStateMap[pAccess][i] = SCE_MBOX_DEFAULT;
+            }
+            continue;
+        }
+
+        if (std::next(data) == documentDataMap[pAccess].end() || std::next(data)->second != SCE_MBOX_BLANK_LINE || std::next(data)->first > endLine) {
+            for (Sci_Position i = begin; i <= end; i++) {
+                documentStateMap[pAccess][i] = SCE_MBOX_DEFAULT;
             }
             continue;
         }
 
         for (Sci_Position i = begin; i <= end; i++) {
-            stateMap[i] = dataMap[i];
+            documentStateMap[pAccess][i] = documentDataMap[pAccess][i];
         } 
 
     }
@@ -269,10 +279,13 @@ void ProcessStates(Sci_Position startLine, Sci_Position endLine) {
 }
 
 // Finds position of last valid MBox header from currentLine
-Sci_Position FindLastMBoxHeader(Sci_Position currentLine) {
+Sci_Position FindLastMBoxHeader(Sci_Position currentLine, IDocument *pAccess) {
 
-    // currentLine - 1 for disable geting negative value if 0 line isn't from
-    while(dataMap.find(currentLine-1) != dataMap.end() && dataMap[currentLine] != SCE_MBOX_BLANK_LINE ) {
+    while(documentDataMap[pAccess].find(currentLine-1) != documentDataMap[pAccess].end() && documentDataMap[pAccess][currentLine] != SCE_MBOX_FROM ) {
+        currentLine--;
+    }
+
+    while(documentDataMap[pAccess].find(currentLine-1) != documentDataMap[pAccess].end() && documentDataMap[pAccess][currentLine] != SCE_MBOX_BLANK_LINE ) {
         currentLine--;
     }
 
@@ -281,21 +294,28 @@ Sci_Position FindLastMBoxHeader(Sci_Position currentLine) {
 
 // Set states for characters
 void SCI_METHOD LexerMBox::Lex(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyle, IDocument *pAccess) {
-    
+
+    if (documentDataMap.find(pAccess) == documentDataMap.end() || documentStateMap.find(pAccess) == documentStateMap.end()) {
+        std::map<Sci_Position, char> dataMap;
+        documentDataMap[pAccess] = dataMap;
+        std::map<Sci_Position, char> stateMap;
+        documentStateMap[pAccess] = stateMap;
+    }
+
     LexAccessor styler(pAccess);
 
-    Sci_Position startLine  = FindLastMBoxHeader(styler.GetLine(startPos));
-    Sci_Position endLine = ProcessLines(startPos, lengthDoc , styler);
-    
+    Sci_Position endLine = ProcessLines(startPos, lengthDoc , styler, pAccess);
+    Sci_Position startLine  = FindLastMBoxHeader(styler.GetLine(startPos), pAccess);
+
     StyleContext scCTX(styler.LineStart(startLine), styler.LineEnd(endLine)-styler.LineStart(startLine), initStyle, styler);
 
-    ProcessStates(startLine, endLine);
+    ProcessStates(startLine, endLine, pAccess);
 
     Sci_Position currentLine = startLine;
-
+    printf("[!] %d -> %d    %d -> %d\n", startLine, endLine, documentDataMap[pAccess][startLine], documentStateMap[pAccess][startLine]);
     for (; scCTX.More() ; scCTX.Forward()) {
         if (scCTX.atLineStart) {
-            scCTX.SetState(stateMap[currentLine]);
+            scCTX.SetState(documentStateMap[pAccess][currentLine]);
         }
         if(scCTX.ch == '\n') {
             currentLine += 1;
@@ -322,16 +342,23 @@ void SCI_METHOD LexerMBox::Lex(Sci_PositionU startPos, Sci_Position lengthDoc, i
 
 void SCI_METHOD LexerMBox::Fold(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyle, IDocument *pAccess) {
     
+    if (documentDataMap.find(pAccess) == documentDataMap.end() || documentStateMap.find(pAccess) == documentStateMap.end()) {
+        std::map<Sci_Position, char> dataMap;
+        documentDataMap[pAccess] = dataMap;
+        std::map<Sci_Position, char> stateMap;
+        documentStateMap[pAccess] = stateMap;
+    }
+    
     LexAccessor styler(pAccess);
-    int baseLevel = styler.LevelAt(stateMap.begin()->first) & SC_FOLDLEVELNUMBERMASK;
+    int baseLevel = styler.LevelAt(documentStateMap[pAccess].begin()->first) & SC_FOLDLEVELNUMBERMASK;
 
-    for (auto line = stateMap.begin(); line != stateMap.end(); line++) {
+    for (auto line = documentStateMap[pAccess].begin(); line != documentStateMap[pAccess].end(); line++) {
         if (line->second == SCE_MBOX_FROM) {
             styler.SetLevel(line->first, baseLevel|SC_FOLDLEVELHEADERFLAG);
         }
         else {
-            auto lookahead = next(line);
-            if (lookahead != stateMap.end() && lookahead->second == SCE_MBOX_FROM) {
+            auto lookahead = std::next(line);
+            if (lookahead != documentStateMap[pAccess].end() && lookahead->second == SCE_MBOX_FROM) {
                 styler.SetLevel(line->first, baseLevel);
             }
             else {
